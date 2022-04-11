b1761d63cc06199c71baf6454b1c9496deba286b
Arailym-Abdrakhym <96182811+Arailym-Abdrakhym@users.noreply.github.com>
2022-04-11 12:03:50 +0600

    Add files via upload

---
+++ Diploma Document/latex.py
@@ -0,0 +1,51 @@
+import subprocess
+import os
+from parsing_all import *
+
+AuthorInf = {'Name' : 'Bogdanchikov Andrey', 'Email' : 'andrey.bogdanchikov@sdu.edu.kz', 'Position':'Associate Professor in Computer Sciences Department', 'Affilation':'Suleyman Demirel University'}
+
+def getbibtext(pub_list):
+    ResText = ''
+    for item in pub_list:
+        ResText = ResText + '\n\item ' + item['Authors'] + ', ' \
+                  + '\href{' + item['Link'] + '}{' + item['Title'] + '}, '\
+                  + item['Where published'] + ', ' \
+                  + item['Year'] + ', pp.' \
+                 # + item['PP.'] + '.'
+    return ResText
+def run():
+    PubList = parsing('Bogdanchikov', 'Andrey')
+    ResText = getbibtext(PubList)
+    #open tex template
+    with open('sometexfile.tex') as template_file:
+        global latex_code
+        latex_code = template_file.read()
+        print(latex_code)
+    # write and save tex file
+    tex_filename = 'restex.tex'
+    with open(tex_filename, 'w') as latex_file:
+        latex_code = latex_code.replace(r'\VAR{NAME}', AuthorInf['Name'])
+        latex_code = latex_code.replace(r'\VAR{EMAIL}', AuthorInf['Email'])
+        latex_code = latex_code.replace(r'\VAR{POS}', AuthorInf['Position'])
+        latex_code = latex_code.replace(r'\VAR{AFF}', AuthorInf['Affilation'])
+        latex_code = latex_code.replace(r'\VAR{PUBLIST}', ResText)
+        latex_file.write(latex_code)
+    print(latex_code)
+    print(f'Generated {tex_filename}')
+
+    '''
+    if os.path.isfile('restex.tex'):
+        print('The file is present.')
+    else:
+        print('The file is not present.')
+
+
+    process = subprocess.Popen([
+        'latex',  # Or maybe 'C:\\Program Files\\MikTex\\miktex\\bin\\latex.exe
+        '-output-format=pdf',
+        '-job-name=' + 'res_pdf.pdf',
+        r'C:/Users/User/Desktop/Diploma Project/restex.tex'])
+    process.wait()
+    '''
+
+run()
