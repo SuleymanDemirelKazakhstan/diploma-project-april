438ba36d106fd03d4245e01dd8294bbffd70f1d5
Arailym-Abdrakhym <96182811+Arailym-Abdrakhym@users.noreply.github.com>
2022-05-24 18:14:37 +0600

    Delete hay directory

--- hay/PythonAPI-master-2/api/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-
-
-urlpatterns = [
-    path('authors', views.getAuthors, name='authors'),
-    path('publications', views.getPublications, name='publications')
-]
