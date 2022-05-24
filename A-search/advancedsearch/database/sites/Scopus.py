19a846f72302d9c892673246dc7211c39266f800
ToktarShynar180103079 <49510585+ToktarShynar180103079@users.noreply.github.com>
2022-05-24 18:18:17 +0600

    Add files via upload

---
+++ A-search/advancedsearch/database/sites/Scopus.py
@@ -0,0 +1,74 @@
+import json
+
+from pybliometrics.scopus import ScopusSearch
+from pybliometrics.scopus import AuthorRetrieval
+from pybliometrics.scopus import AuthorSearch
+from pybliometrics.scopus.exception import Scopus401Error
+from ..models.Author import *
+from ..models.Publication import *
+
+
+def scopus_author(lastname, name, result, index):
+    res_aut_list = []
+    try:
+        s = AuthorSearch(f'AUTHLAST({lastname}) and AUTHFIRST({name})')
+        res = s.authors
+        for i in range(len(res)):
+            author = Author(
+                index = i,
+                name = str(res[i][4].split()[0]),
+                surname= str(res[i][2]),
+                orcidId= str(res[i][1]),
+                scopusId=res[i][0][7:],
+                docCount=str(res[i][6]),
+                affilation=str(res[i][5]),
+                country= str(res[i][8])  + ', ' + str(res[i][9]),
+                area=str(res[i][10])
+            )
+            res_aut_list.append(author)
+    except:
+        result[index] = res_aut_list
+
+    result[index] = res_aut_list
+
+def scopus_pub(scopusId):
+    result = []
+    try:
+        au = AuthorRetrieval(scopusId)
+        auth = Author(
+            name=au.given_name,
+            surname=au.surname,
+            orcidId=au.orcid,
+            scopusId=scopusId,
+            docCount=au.document_count,
+            affilation=au.affiliation_current[0][5],
+            country=au.affiliation_current[0][7]
+        )
+        result.append(auth)
+        public = []
+        a = au.get_documents()
+        for i in range(len(a)):
+            publication = Publication(
+                site="Scopus",
+                authors=a[i][13],
+                title=a[i][4],
+                type=a[i][6],
+                link=None,
+                year=a[i][17],
+                wherePublished=a[i][18],
+                PP=a[i][26],
+                volume=a[i][23],
+                keyWords=a[i][28]
+            )
+            public.append(publication)
+        result.append(public)
+    except Scopus401Error:
+        return result
+    return result
+
+# authors = scopus_author("Ba", "") # 3
+
+# print(scopus_pub(55832104800))
+
+# for i in range(len(list)):
+#     print(list[i].__str__())
